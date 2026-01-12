13404641318c9e622a3d48860c820258e1256eca
Aswin <aswin952003@gmail.com>
2026-01-12 13:06:32 +0530

    updated and changed to versel deployment

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const dontenv = require("dotenv");
 dontenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", require("./routes/heroRoutes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>{
-    console.log(`Server running on ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on ${PORT}`)});
+
+module.exports=app;
