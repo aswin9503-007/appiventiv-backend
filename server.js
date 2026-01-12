c73c9a9ebc3779edd8ae3ab1f61ab2ef862b3c66
Aswin <aswin952003@gmail.com>
2026-01-12 16:54:48 +0530

    updated sere and versel

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv"); 
 
 dotenv.config();
 
@@ -11,5 +11,15 @@ app.use(express.json());
 
 app.use("/api", require("./routes/heroRoutes"));
 
+app.get("/", (req, res) => {
+  res.send("Appinventiv Backend is running...");
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
 
 module.exports = app;
