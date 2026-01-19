80c620802c7d0f3a234b0d2784fd6a4cd6ed815b
Aswin <aswin952003@gmail.com>
2026-01-19 15:28:32 +0530

    engineeringServiceRoutes updated

--- routes/engineeringServiceRoutes.js
+++ routes/engineeringServiceRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../controllers/serviceController");
+const ctrl = require("../controllers/engineeringServiceController");
 
 router.get("/", ctrl.getAllServices);
 router.post("/create", ctrl.createService);
